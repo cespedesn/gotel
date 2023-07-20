4a1cf8b7578ca240ab2f9c51e3890e1fab7065ba
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2023-07-20 13:37:53 -0400

    Adjusted css for hotel info and green program

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ class App extends React.Component {
             </article>
             <article id="greenprogram">
               <h2>Gotel Green Program</h2>
-              <p><strong>The Gotel Hotel - London</strong> was recently renovated, and we considered the impact on the earth the entire way. From green building materials, to solar power, to energy-friendly lighting and appliances throughout the hotel - we’re saving energy in every socket, outlet, and switch. We’ve also initiated a recycling and composting program that reduces the load to local landfills, while providing valuable raw material for use in new products, or in the case of compost, for use in local gardens and landscapes.</p>
+              <p><strong>The Gotel Hotel - SoHo</strong> recently underwent a renovation, and the environment was taken into consideration at every step. With energy-saving lighting and appliances, green building materials, and solar power - we're saving energy in every socket, outlet, and switch. In addition, we've started a recycling and composting program that reduces waste going to landfills, while providing valuable raw materials for new products or for use as compost in local gardens and landscapes.</p>
             </article>
           </div>
         </main>
