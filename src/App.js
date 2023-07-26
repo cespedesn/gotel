036052faed222a885db03d26b16d1321812b2f27
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2023-07-26 13:36:22 -0400

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './components/Header';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,28 +7,7 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <header id="intro">
-          <article class="fullheight">
-            <div class="hgroup">
-              <h1>Gotel Hotel</h1>
-              <h2>SoHo</h2>
-              <p><a href="#welcome"><img src="https://landonhotel.com/images/misc/arrow.png" alt="down arrow"/></a></p>
-            </div>
-          </article>
-
-          <nav id="nav">
-            <div className="navbar">
-              <div className="brand"><a href="#welcome">Gotel <span>Hotel</span></a></div>
-              <ul>
-                <li><a class="icon info" href="#hotelinfo"><span>info</span></a></li>
-                <li><a className="icon rooms" href="#rooms"><span>rooms</span></a></li>
-                <li><a className="icon dining" href="#dining"><span>dining</span></a></li>
-                <li><a className="icon events" href="#events"><span>events</span></a></li>
-                <li><a className="icon attractions" href="#attractions"><span>attractions</span></a></li>
-              </ul>
-            </div>
-          </nav>
-        </header>
+        <Header />
 
         <main id="wrapper">
           <div className="scene" id="welcome">
